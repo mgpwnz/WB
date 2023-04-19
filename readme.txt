f6181a932cdecfc8120030b0a3c37980ba86a8e2
Vitaliy <95574118+mgpwnz@users.noreply.github.com>
2023-04-19 13:34:49 +0300

    test

---
+++ readme.txt
@@ -0,0 +1,1 @@
+. <(wget -qO- https://raw.githubusercontent.com/mgpwnz/wb/main/wb.sh)
---
+++ wb.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+# Default variables
+function="install"
+# Options
+option_value(){ echo "$1" | sed -e 's%^--[^=]*=%%g; s%^-[^=]*=%%g'; }
+while test $# -gt 0; do
+        case "$1" in
+        -in|--install)
+            function="install"
+            shift
+            ;;
+        -un|--uninstall)
+            function="uninstall"
+            shift
+            ;;
+		break
+		;;
+	esac
+done
+install() {
+#docker install
+cd
+touch $HOME/.bash_profile
+if ! docker --version; then
+		echo -e "${C_LGn}Docker installation...${RES}"
+		sudo apt update
+		sudo apt upgrade -y
+		sudo apt install curl apt-transport-https ca-certificates gnupg lsb-release -y
+		. /etc/*-release
+		wget -qO- "https://download.docker.com/linux/${DISTRIB_ID,,}/gpg" | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+		echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/${DISTRIB_ID,,} ${DISTRIB_CODENAME} stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+		sudo apt update
+		sudo apt install docker-ce docker-ce-cli containerd.io -y
+		docker_version=`apt-cache madison docker-ce | grep -oPm1 "(?<=docker-ce \| )([^_]+)(?= \| https)"`
+		sudo apt install docker-ce="$docker_version" docker-ce-cli="$docker_version" containerd.io -y
+	fi
+	if ! docker-compose --version; then
+		echo -e "${C_LGn}Docker Сompose installation...${RES}"
+		sudo apt update
+		sudo apt upgrade -y
+		sudo apt install wget jq -y
+		local docker_compose_version=`wget -qO- https://api.github.com/repos/docker/compose/releases/latest | jq -r ".tag_name"`
+		sudo wget -O /usr/bin/docker-compose "https://github.com/docker/compose/releases/download/${docker_compose_version}/docker-compose-`uname -s`-`uname -m`"
+		sudo chmod +x /usr/bin/docker-compose
+		. $HOME/.bash_profile
+	fi
+cd $HOME
+#create dir and config
+if [ ! -d $HOME/whitebit ]; then
+mkdir $HOME/whitebit
+fi
+cd $HOME/whitebit
+sleep 1
+ # Create script 
+  tee $HOME/subspace/docker-compose.yml > /dev/null <<EOF
+  version: "3.7"
+  name: whitebit
+
+services:
+  fullnode:
+    image: whitebit/wbt:0.1.0
+    restart: always
+    command: ["--wbt-testnet --http.addr 0.0.0.0"]
+    ports:
+    - '8545:8545'
+    - '30303:30303'
+    volumes:
+    - ${HOME}/whitebit/data:/root
+
+volumes:
+  data:
+
+EOF
+sleep 2
+#docker run
+docker compose up -d && docker compose logs -f --tail 1000
+}
+uninstall() {
+cd $HOME/whitebit
+docker compose down -v
+sudo rm -rf $HOME/whitebit 
+echo "Done"
+cd
+}
+# Actions
+sudo apt install wget -y &>/dev/null
+cd
+$function
